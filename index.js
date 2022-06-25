17301ac39294593bf3a653796edb0fc443db2081
hawko666 <61893709+hawko666@users.noreply.github.com>
2022-06-25 14:24:18 +0530

    emoji

---
+++ index.js
@@ -0,0 +1,11 @@
+// js code
+//console.log('Hello world');
+//let name = 'man';
+//console.log(name); 
+/*let selectedColors = ['red', 'blue'];
+console.log(selectedColors[0]);*/
+function greet(name) {
+    console.log('hello ' + name);
+}
+
+greet('john')
